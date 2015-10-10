60ba405b21dfeb893890609a8ad9a4df5e7c5328
whitetiger2015 <bulldogshepherd@gmail.com>
2015-10-11 04:20:23 +0900

    Modified header with read_num.

--- rename_fastq_header.py
+++ rename_fastq_header.py
@@ -5,6 +5,13 @@ import sys
 
 if __name__ == "__main__":
     line_num = 0
+    read_num = 0
     for line in iter(sys.stdin.readline, ""):
         line_num += 1
-        print line,
+        if line.startswith('@'):
+            read_num += 1
+            header = line.rstrip("\n")
+            header = header + '_' + str(read_num)
+            print header
+        else:
+            print line,
