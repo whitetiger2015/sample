2e658282abae0334435381c666b9f01da2359c80
whitetiger2015 <bulldogshepherd@gmail.com>
2015-10-11 05:58:16 +0900

    Added arguments inclusion to header.

--- rename_fastq_header.py
+++ rename_fastq_header.py
@@ -4,6 +4,9 @@
 import sys
 
 if __name__ == "__main__":
+    header_arg = []
+    for a in sys.argv[1:]:
+        header_arg.append(a)
     line_num = 0
     read_num = 0
     for line in iter(sys.stdin.readline, ""):
@@ -11,7 +14,10 @@ if __name__ == "__main__":
         if line.startswith('@'):
             read_num += 1
             header = line.rstrip("\n")
-            header = header + '_' + str(read_num)
-            print header
+            header_new = []
+            header_new.append(header)
+            header_new.extend(header_arg)
+            header_new.append(str(read_num))
+            print "_".join(header_new)
         else:
             print line,
